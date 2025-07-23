f9c7feb5e822bc9c5315749be31a7cc7b58e0461
Jonas Franz <jonas.franz@zollsoft.de>
2025-07-23 15:57:43 +0200

    improve search bar

--- lib/screens/search/map_search_bar.dart
+++ lib/screens/search/map_search_bar.dart
@@ -17,6 +17,7 @@ class MapSearchBar extends HookWidget {
 
   @override
   Widget build(BuildContext context) {
+    final focusNode = useFocusNode();
     final viewController = useMemoized<MapSearchBarController>(getIt.call);
     final searchController = useSearchController();
     useEffect(() {
@@ -28,13 +29,30 @@ class MapSearchBar extends HookWidget {
       children: [
         ConstrainedBox(
           constraints: BoxConstraints(maxWidth: 720),
-          child: SearchAnchor.bar(
+          child: SearchAnchor(
             isFullScreen: false,
             textInputAction: TextInputAction.done,
             shrinkWrap: true,
-            barTrailing: [
-              IconButton(onPressed: onGoToCurrentLocation, icon: Icon(Icons.my_location)),
-            ],
+            builder:
+                (context, controller) => SearchBar(
+                  padding: const WidgetStatePropertyAll<EdgeInsets>(
+                    EdgeInsets.symmetric(horizontal: 16.0),
+                  ),
+
+                  focusNode: focusNode,
+                  controller: controller,
+                  trailing: [
+                    IconButton(onPressed: onGoToCurrentLocation, icon: Icon(Icons.my_location)),
+                  ],
+                  leading: const Icon(Icons.search),
+
+                  onTap: () {
+                    controller.openView();
+                  },
+                  onChanged: (_) {
+                    controller.openView();
+                  },
+                ),
             searchController: searchController,
             suggestionsBuilder: (context, controller) async {
               final results = await viewController.findSuggestions();
@@ -48,9 +66,7 @@ class MapSearchBar extends HookWidget {
                   onTap: () {
                     onSelectLocation(result.position);
                     controller.closeView("");
-                    WidgetsBinding.instance.addPostFrameCallback((_) {
-                      FocusScope.of(context).unfocus();
-                    });
+                    focusNode.unfocus();
                   },
                 ),
               );
